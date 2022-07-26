246bc65257cff164732658837e19a88d27053529
devcarolinealmeida <carolineitalia85@gmail.com>
2022-07-26 18:03:14 +0200

    adicionando funcao valida email

--- main.js
+++ main.js
@@ -1,32 +1,95 @@
 //valida email
 
-const inputEmail = document.querySelector("#email")
-const btnEmail = document.querySelector("#btn-email")
-const msgErro = document.querySelector("#mensagem-erro")
+const btnEmail = document.querySelectorAll("[data-btn]")
+const inputEmail = document.querySelectorAll("[data-email]")
+const msgErro = document.querySelectorAll("[data-msg]")
 
 
-btnEmail.addEventListener("click", (evento) => {
-    evento.preventDefault()
-    if (inputEmail.value == "") {
-        msgErro.classList.toggle("invisivel")
-        msgErro.classList.toggle("visivel-erro")
-        msgErro.classList.remove("visivel-sucesso")
-        inputEmail.classList.toggle("input-mensagem-erro")
-        msgErro.textContent = "Please enter your email"
-    } else if (!validaEmail(inputEmail.value) == true) {
-        msgErro.classList.toggle("invisivel")
-        msgErro.classList.add("visivel-erro")
-        msgErro.classList.remove("visivel-sucesso")
-        msgErro.textContent = "Enter a valid email"
+function verificaInput(posicao){
+    if (posicao === "1") {
+        let i = 0;
+        if (inputEmail[i].value == "") {
+            msgErro[i].classList.toggle("invisivel")
+            msgErro[i].classList.toggle("visivel-erro")
+            msgErro[i].classList.remove("visivel-sucesso")
+            inputEmail[i].classList.toggle("input-mensagem-erro")
+            msgErro[i].textContent = "Please enter your email"
+        } else if (!validaEmail(inputEmail[i].value) == true) {
+            msgErro[i].classList.toggle("invisivel")
+            msgErro[i].classList.add("visivel-erro")
+            msgErro[i].classList.remove("visivel-sucesso")
+            msgErro[i].textContent = "Enter a valid email"
+        } else {
+            msgErro[i].classList.toggle("visivel-sucesso")
+            inputEmail[i].classList.remove("input-mensagem-erro")
+            inputEmail[i].value = ""
+            msgErro[i].textContent = "Thanks for contacting us! We will be in touch with you shortly."
+        }
     } else {
-        msgErro.classList.toggle("visivel-sucesso")
-        inputEmail.classList.remove("input-mensagem-erro")
-        inputEmail.value = ""
-        msgErro.textContent = "Thanks for contacting us! We will be in touch with you shortly."
+        let i = 1;
+        if (inputEmail[i].value == "") {
+            msgErro[i].classList.toggle("invisivel")
+            msgErro[i].classList.toggle("visivel-erro")
+            msgErro[i].classList.remove("visivel-sucesso")
+            inputEmail[i].classList.toggle("input-mensagem-erro")
+            msgErro[i].textContent = "Please enter your email"
+        } else if (!validaEmail(inputEmail[i].value) == true) {
+            msgErro[i].classList.toggle("invisivel")
+            msgErro[i].classList.add("visivel-erro")
+            msgErro[i].classList.remove("visivel-sucesso")
+            msgErro[i].textContent = "Enter a valid email"
+        } else {
+            msgErro[i].classList.toggle("visivel-sucesso")
+            inputEmail[i].classList.remove("input-mensagem-erro")
+            inputEmail[i].value = ""
+            msgErro[i].textContent = "Thanks for contacting us! We will be in touch with you shortly."
+        }
     }
+}
+
+btnEmail.forEach((botao) => {
+    botao.addEventListener("click", (evento) => {
+        evento.preventDefault()
+        verificaInput(evento.target.dataset.btn)
+    })
 })
+
 
 function validaEmail(email) {
     let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
     return emailPattern.test(email)
 }
+
+
+// //valida email versao anterior
+
+// const inputEmail = document.querySelector("#email")
+// const btnEmail = document.querySelector("#btn-email")
+// const msgErro = document.querySelector("#mensagem-erro")
+
+
+// btnEmail.addEventListener("click", (evento) => {
+//     evento.preventDefault()
+//     if (inputEmail.value == "") {
+//         msgErro.classList.toggle("invisivel")
+//         msgErro.classList.toggle("visivel-erro")
+//         msgErro.classList.remove("visivel-sucesso")
+//         inputEmail.classList.toggle("input-mensagem-erro")
+//         msgErro.textContent = "Please enter your email"
+//     } else if (!validaEmail(inputEmail.value) == true) {
+//         msgErro.classList.toggle("invisivel")
+//         msgErro.classList.add("visivel-erro")
+//         msgErro.classList.remove("visivel-sucesso")
+//         msgErro.textContent = "Enter a valid email"
+//     } else {
+//         msgErro.classList.toggle("visivel-sucesso")
+//         inputEmail.classList.remove("input-mensagem-erro")
+//         inputEmail.value = ""
+//         msgErro.textContent = "Thanks for contacting us! We will be in touch with you shortly."
+//     }
+// })
+
+// function validaEmail(email) {
+//     let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+//     return emailPattern.test(email)
+// }
