f40f7e2c91a2a353759e6f4f9f3573688bcc9183
devcarolinealmeida <carolineitalia85@gmail.com>
2022-07-28 11:44:02 +0200

    finalizando funçao verificaInput

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//valida email
+//valida campo email e mostra mensagens de erro
 
 const btnEmail = document.querySelectorAll("[data-btn]")
 const inputEmail = document.querySelectorAll("[data-email]")
@@ -13,27 +13,7 @@ btnEmail.forEach((botao) => {
 })
 
 function verificaInput(posicao){
-    if (posicao === "0") {
-        let i = posicao;
-        if (inputEmail[i].value == "") {
-            msgErro[i].classList.toggle("invisivel")
-            msgErro[i].classList.add("visivel-erro")
-            msgErro[i].classList.remove("visivel-sucesso")
-            inputEmail[i].classList.add("input-mensagem-erro")
-            msgErro[i].textContent = "Please enter your email"
-        } else if (!validaEmail(inputEmail[i].value) == true) {
-            msgErro[i].classList.toggle("invisivel")
-            msgErro[i].classList.add("visivel-erro")
-            msgErro[i].classList.remove("visivel-sucesso")
-            msgErro[i].textContent = "Enter a valid email"
-        } else {
-            msgErro[i].classList.toggle("visivel-sucesso")
-            inputEmail[i].classList.remove("input-mensagem-erro")
-            inputEmail[i].value = ""
-            msgErro[i].textContent = "Thanks for contacting us! We will be in touch with you shortly."
-        }
-    } else {
-        let i = posicao;
+        const i = posicao;
         if (inputEmail[i].value == "") {
             msgErro[i].classList.toggle("invisivel")
             msgErro[i].classList.add("visivel-erro")
@@ -52,7 +32,6 @@ function verificaInput(posicao){
             msgErro[i].textContent = "Thanks for contacting us! We will be in touch with you shortly."
         }
     }
-}
 
 function validaEmail(email) {
     let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
